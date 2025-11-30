be6613f60bbb90a1bc80b070e6704a9d4713d4ba
mohamedzainb2-eng <mohamed.noda.b2@gmail.com>
2025-12-01 01:16:31 +0200

    Build docs folder for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  // 👇 مهم علشان GitHub Pages /real-estate-ui/
   base: "/real-estate-ui/",
+  build: {
+    outDir: "docs", // 👈 خلي Vite يبني في docs مباشرة
+  },
 });
